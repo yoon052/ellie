826ec8051bf4c11b532a7a2c201648876452e59c
Yoon <Yoon@naver.com>
2023-05-06 01:26:36 +0900

    feat : variable.js 코드 추가2
    variable.js : primitive 타입, object 타입 변수와 dynamic typed language 에 관한 내용 추가됨

--- variable.js
+++ variable.js
@@ -13,7 +13,7 @@ let globalName = "global name";
   console.log(name);
   console.log(globalName);
 }
-console.log(name);
+
 console.log(globalName);
 
 // var (don't ever use this!)
@@ -35,6 +35,72 @@ const daysInWeek = 7;
 const maxNumber = 5;
 
 // 4. Variable types
-// primitive, single item : number, string, boolean, null, undefined
+// primitive, single item : number, string, boolean, null, undefined, symbol
 // object, box container
 // function, first-class function
+
+const count = 17; //integer
+const size = 17.1; //decimal number
+console.log(`value : ${count}, type : ${typeof count}`);
+console.log(`value : ${size}, type : ${typeof size}`);
+
+// number - special numberic values : infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = "not a number" / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet)
+const bigInt = 1234567890123456789012345678901234567890n; // over (-2**53 ~ 2**53)
+console.log(`value : ${bigInt}, type : ${typeof bigInt}`);
+Number.MAX_SAFE_INTEGER;
+
+// string
+const char = "c";
+const brendan = "brendan";
+const greeting = "hello " + brendan;
+console.log(`value : ${greeting}, type : ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; //template literals (string)
+console.log(`value : ${helloBob}, type : ${typeof helloBob}`);
+
+// boolean
+// false : 0, null, undefined, NaN, ''
+// true : any other value
+const canRead = true;
+const test = 3 < 1; // false;
+console.log(`value : ${canRead}, type : ${typeof canRead}`);
+console.log(`value : ${test}, type : ${typeof test}`);
+
+// null
+let nothing = null;
+console.log(`value : ${nothing}, type : ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value : ${x}, type : ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol("id");
+const symbol2 = Symbol("id");
+console.log(symbol1 === symbol2); //false
+const gSymbol1 = Symbol.for("id");
+const gSymbol2 = Symbol.for("id");
+console.log(gSymbol1 === gSymbol2); //true
+console.log(
+  `value : ${symbol1.description}, type : ${typeof symbol1.descriptionN}`
+);
+
+// object, real-life object, data structure
+const ellie = { name: "ellie", age: 20 };
+
+// 5. Dynamic typing : dynamically typed language
+let text = "hello";
+console.log(`value : ${text}. type : ${typeof text}`);
+text = 1;
+console.log(`value : ${text}. type : ${typeof text}`);
+text = "7" + 5;
+console.log(`value : ${text}. type : ${typeof text}`);
+text = "8" / "2";
+console.log(`value : ${text}. type : ${typeof text}`);
