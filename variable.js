6984533bbf31dcf97c0aa8db598bd3136dad36cb
Yoon <Yoon@naver.com>
2023-05-05 22:42:09 +0900

    feat : variable.js 코드 추가
    variable.js : 데이터 타입과 변수 특성과 이용에 관한 내용

--- variable.js
+++ variable.js
@@ -5,8 +5,36 @@
 
 // 2. Variable
 // let (added in ES6)
+let globalName = "global name";
+{
+  let name = "ellie"; //name 을 let으로 선언함과 동시에 "ellie" 라는 값을 할당함
+  console.log(name);
+  name = "hello"; // 재할당하여 name에 "hello" 라는 값이 할당됨
+  console.log(name);
+  console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
 
-let name = "ellie"; //name 을 let으로 선언함과 동시에 "ellie" 라는 값을 할당함
-console.log(name);
-name = "hello";
-console.log(name);
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+{
+  age = 4;
+  var age;
+}
+console.log(age);
+
+// 3. Constant
+// facor immutable data type always for a few reasons:
+// - security
+// - thread safety
+// - reduce human mistakes
+
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// 4. Variable types
+// primitive, single item : number, string, boolean, null, undefined
+// object, box container
+// function, first-class function
