03df298b0c7ec2af2d911c00e13bad74e0728a88
Yoon <Yoon@naver.com>
2023-05-05 09:29:11 +0900

    Feat : add variable.js
    variable.js 파일 추가

---
+++ variable.js
@@ -0,0 +1,12 @@
+// 1. Use strict
+// added in
+// use this in Vanila Javascript
+"use strict";
+
+// 2. Variable
+// let (added in ES6)
+
+let name = "ellie"; //name 을 let으로 선언함과 동시에 "ellie" 라는 값을 할당함
+console.log(name);
+name = "hello";
+console.log(name);
