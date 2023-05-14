0c5caa784c4dbef843d595cfb535960d6dd3d395
Yoon <Yoon@naver.com>
2023-05-14 22:54:18 +0900

    feat : object.js 코드 추가
    object 생성/추가/삭제, 프로퍼티, in/of 연산자, object 복제 개념 정리

---
+++ function.js
@@ -0,0 +1,11 @@
+// Function
+// -fundamental building block in the program
+// - subprogram can be used multiple times
+// - performs a task or calculates a value
+
+// 1. Function declaration
+// function name(param1, param2) { body... return; }
+// one function === one thing
+// naming : doSomething, command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
